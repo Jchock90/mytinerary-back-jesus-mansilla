02a0f5550bfd4710568d58bee0e51caf057b9e71
Jesus <jchockmansilla@gmail.com>
2023-09-12 22:55:44 -0300

    server v.4

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import usersRouter from './users.js'
 import citiesRouter from './cities.js'
 import itineriesRouter from './itineraries.js'
 import activitiesRouter from './activities.js'
+import authRouter from './auth.js';
 
 let router = express.Router();
 
@@ -10,5 +11,6 @@ router.use('/users', usersRouter)
 router.use('/cities', citiesRouter)
 router.use('/itineraries', itineriesRouter)
 router.use('/activities',activitiesRouter )
+router.use('/auth',authRouter)
 
 export default router;
